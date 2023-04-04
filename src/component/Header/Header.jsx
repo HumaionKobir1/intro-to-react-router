793d0124a6fbd5240708ecad07d7145783bd380c
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-04 16:31:15 +0600

    Nested route and outler for shared contact

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,37 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './component/About/About';
+import Contact from './component/Contact/Contact';
+import Home from './component/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App></App>
-  },
-  {
-    path: '/about',
-    element: <div>This is about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>This is contact page</div>
-  },
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
+  }
+  // {
+  //   path: '/',
+  //   element: <App></App>
+  // },
+  // {
+  //   path: '/about',
+  //   element: <About></About>
+  // },
+  // {
+  //   path: '/contact',
+  //   element: <Contact></Contact>
+  // },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
